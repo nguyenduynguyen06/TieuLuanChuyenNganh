a45b73035f4d0b6906ebce73bf8339b52c339488
Nguyễn Duy Nguyễn <20110530@student.hcmute.edu.vn>
2023-10-12 00:43:20 +0700

    update

--- client/src/Components/ListBrandComponent/ListBrandComponent.jsx
+++ client/src/Components/ListBrandComponent/ListBrandComponent.jsx
@@ -1,36 +1,37 @@
-import React from "react";
+import React, {useEffect,useState} from "react";
 import { WrapperBrandList } from "./style";
-
+import axios from "axios";
 const ListBrand = () => {
+    const [brand, setBrands] = useState([]);
+
+    useEffect(() => {
+  
+      const fetchCategories = async () => {
+        try {
+          const response = await axios.get(`${process.env.REACT_APP_API_URL}/brand/getBrand`);
+          setBrands(response.data.data);
+        } catch (error) {
+          console.error('Lỗi khi lấy danh sách danh mục:', error);
+        }
+      };
+      fetchCategories();
+    }, []);
+  
+    const handleCardClick = (name) => {
+      window.location.href = `/type/${name}`;
+    };
     return (
         <WrapperBrandList style={{}}>
+        {brand.filter((brands) => brands.isHide === false).map((brands) => (
             <div className="brand-content">
                 <div className="list-brand">
                     <a className="list-brand-item" href="#">
-                        <img className="brand-img" src="https://www.pngmart.com/files/10/Apple-Logo-PNG-Photos.png" alt="Apple" />
-                    </a>
-                    <a className="list-brand-item" href="#">
-                        <img className="brand-img" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Samsung_Logo.svg/1024px-Samsung_Logo.svg.png" alt="Samsung" />
-                    </a>
-                    <a className="list-brand-item" href="#">
-                        <img className="brand-img" src="https://assets.stickpng.com/images/60410c7b26ef2b00045692f8.png" alt="Xiaomi" />
-                    </a>
-                    <a className="list-brand-item" href="#">
-                        <img className="brand-img" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/OPPO_LOGO_2019.svg/2560px-OPPO_LOGO_2019.svg.png" alt="Oppo" />
-                    </a>
-                    <a className="list-brand-item" href="#">
-                        <img className="brand-img" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Redmi_Logo.svg/2560px-Redmi_Logo.svg.png" alt="Redmi" />
-                    </a>
-                    <a className="list-brand-item" href="#">
-                        <img className="brand-img" src="https://companieslogo.com/img/orig/NOK_BIG-8604230c.png?t=1677607482" alt="Nokia" />
-                    </a>
-                    <a className="list-brand-item" href="#">
-                        <img className="brand-img" src="https://logonoid.com/images/huawei-logo.png" alt="Huawei" />
+                        <img className="brand-img" src={brands.picture} alt="Apple" />
                     </a>
                 </div>
             </div>
+  ))}
         </WrapperBrandList>
-
     )
 }
 
