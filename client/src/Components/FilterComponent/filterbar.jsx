8e5683cd180d972a00330a810ca344df4d7db5cd
QuangDuongLe <20110454@student.hcmute.edu.vn>
2023-10-19 01:52:13 +0700

    updateTypeProductPage

---
+++ client/src/Components/FilterComponent/filterbar.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { DownOutlined , FallOutlined ,RiseOutlined} from '@ant-design/icons';
+import { Button, Checkbox, Dropdown,Menu, Space } from 'antd';
+import { WrapperFilterList } from './style';
+
+const FilterBar = () => {
+    const menu1 = (
+        <Menu>
+            <Menu.Item key="1">Dưới 2 triệu</Menu.Item>
+            <Menu.Item key="2">Từ 2 đến 4 triệu</Menu.Item>
+            <Menu.Item key="3">Từ 4 đến 7 triệu</Menu.Item>
+            <Menu.Item key="4">Từ 7 đến 13 triệu</Menu.Item>
+            <Menu.Item key="5">Từ 13 đến 20 triệu</Menu.Item>
+            <Menu.Item key="6">Trên 20 triệu</Menu.Item>
+        </Menu>
+    );
+
+    const menu2 = (
+        <Menu>
+            <Menu.Item key="5">64GB đến 128GB</Menu.Item>
+            <Menu.Item key="6">256GB đến 512GB</Menu.Item>
+            <Menu.Item key="7">Trên 512GB</Menu.Item>
+        </Menu>
+    );
+    const menu3 = (
+        <Menu>
+            <Menu.Item key="8">5 sao</Menu.Item>
+            <Menu.Item key="9">4 sao</Menu.Item>
+            <Menu.Item key="10">3 sao</Menu.Item>
+            <Menu.Item key="11">2 sao</Menu.Item>
+            <Menu.Item key="12">1 sao</Menu.Item>
+
+        </Menu>
+    );
+   return (
+        <WrapperFilterList>
+            <Button style={{ display: 'inline-flex', alignItems: 'center' }}>
+            <FallOutlined style={{ verticalAlign: 'middle', marginRight: '5px' }}/>Giá Cao - Thấp</Button>
+            <Button style={{ display: 'inline-flex', alignItems: 'center' }}> 
+            <RiseOutlined style={{ verticalAlign: 'middle', marginRight: '5px' }}/>Giá Thấp - Cao</Button>
+            <Dropdown overlay={menu1} placement="bottomLeft">
+                <a className="ant-dropdown-link" onClick={(e) => e.preventDefault()}>
+                    Giá <DownOutlined />
+                </a>
+            </Dropdown>
+            <Dropdown overlay={menu2} placement="bottomLeft">
+                <a className="ant-dropdown-link" onClick={(e) => e.preventDefault()}>
+                    Bộ nhớ <DownOutlined />
+                </a>
+            </Dropdown>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+        <Dropdown overlay={menu3} placement="bottomLeft">
+          <a className="ant-dropdown-link" onClick={(e) => e.preventDefault()}>
+            Số sao <DownOutlined />
+          </a>
+        </Dropdown>
+        <Checkbox style={{ marginLeft: '10px' }}>Giảm giá</Checkbox>
+      </div>
+
+
+        </WrapperFilterList>
+    )
+}
+
+export default FilterBar;
--- client/src/pages/ProductTypePage/producttypepage.jsx
+++ client/src/pages/ProductTypePage/producttypepage.jsx
@@ -1,34 +1,53 @@
-import React, { useEffect }  from "react";
+import React, { useEffect } from "react";
 import NavBarComponent from "../../Components/NavBarComponent/navbarcomponent";
 import CardComponent from "../../Components/CardComponent/CardComponent";
 import { Col, Row } from 'antd'
 import ListBrand from "../../Components/ListBrandComponent/ListBrandComponent";
 import Header from "../../Components/Header/header";
 import FilterCard from "../../Components/CardComponent/filtercard";
+import { FilterBAndW } from "@mui/icons-material";
+import Slide from "../../Components/Slider/Slide"
+import FilterBar from "../../Components/FilterComponent/filterbar";
+import CateList from "../../Components/FilterComponent/catelist";
+import { WrapperType } from "./style";
 
 
 const TypeProductPage = () => {
-    useEffect(()=>{
-        window.scrollTo({top:0,behavior: "instant"})
-    },[])
+    useEffect(() => {
+        window.scrollTo({ top: 0, behavior: "instant" })
+    }, [])
     return (
-        <div>
-            <Header />
-            <div style={{ background: '#fff', flexWrap: 'nowrap', paddingTop: '10px' }}>
-                <Row >
-                    <ListBrand/>
-                </Row>
-                <Row style={{ background: '#fff', marginRight: '10px', padding: '10px', borderRadius: '6px' }}>
-                    <Col span={4} style={{paddingLeft: '20px'}}>
-                        <NavBarComponent />
-                    </Col>
-                    <Col span={20}>
-                        <CardComponent/>
-                    </Col>
-                </Row>
-                <br></br>
-            </div>
-        </div>
+        <WrapperType>
+            <Row>
+                <Header />
+            </Row>
+            <Row>
+                <Col style={{ width: '5%', paddingLeft: '10px', overflow: 'hidden' }}>
+                </Col>
+                <Col style={{ width: '89%', paddingLeft: '10px', paddingRight: '10px' }}>
+                    <Row>
+                        <br></br>
+                        <Slide ></Slide>
+                        <br></br>
+                        <CateList />
+                        <br></br>
+                        <ListBrand />
+                        <br></br>
+                        <FilterBar />
+                        <br></br>
+                    </Row>
+                    <Row style={{ background: '#fff', marginRight: '10px', padding: '10px', borderRadius: '6px' }}>
+                        <Col span={24}>
+                            <CardComponent />
+                        </Col>
+                    </Row>
+                    <br></br>
+                </Col>
+                <Col style={{ width: '5%', paddingLeft: '10px', overflow: 'hidden' }}>
+                </Col>
+            </Row>
+
+        </WrapperType>
 
     )
 }
