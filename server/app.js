a188c4d07ea31a27602c9b84a327083ec8542c51
Nguyễn Duy Nguyễn <20110530@student.hcmute.edu.vn>
2023-10-19 19:55:31 +0700

    update

--- server/app.js
+++ server/app.js
@@ -9,17 +9,18 @@ const routerBrand = require('./Router/BrandRouter')
 const routerCart = require('./Router/CartRouter')
 const routerComment = require('./Router/CommentRouter')
 const bodyParser = require('body-parser');
+const path = require('path')
 const routerCategory = require('./Router/CategoryRouter')
 const server = require('http').createServer(app);
 const cookieParser = require('cookie-parser')
 const cors = require('cors');
 
 
-
 app.use(cors({
     origin: true, 
     credentials: true, 
   }));
+app.use(cors());
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Credentials', true);
     next();
@@ -28,15 +29,22 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/', routerUpload);
-app.use('/user', routerUser);
-app.use('/product', routerProduct);
-app.use('/category',routerCategory);
-app.use('/comment',routerComment);
-app.use('/brand',routerBrand)
-app.use('/cart',routerCart)
+app.use('/api', routerUpload);
+app.use('/api/user', routerUser);
+app.use('/api/product', routerProduct);
+app.use('/api/category',routerCategory);
+app.use('/api/comment',routerComment);
+app.use('/api/brand',routerBrand)
+app.use('/api/cart',routerCart)
+if(process.env.NODE_ENV === 'production')
+{
+    app.use(express.static('./client/build'));
+    app.get("*",(req,res) => {
+        res.sendFile(path.resolve(__dirname,'./client',"build",'index.html'))
+    })
+}
 connect2DB();
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT 
 server.listen(PORT, () => {
     console.log(`Server is now running on PORT: ${PORT}`);
 })
