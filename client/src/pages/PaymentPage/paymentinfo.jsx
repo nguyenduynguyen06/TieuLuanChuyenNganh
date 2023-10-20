2eea6b781bb85a65b7691f551ebc3efea50d5e0d
QuangDuongLe <20110454@student.hcmute.edu.vn>
2023-10-20 18:33:30 +0700

    updatePayment

--- client/src/pages/PaymentPage/paymentinfo.jsx
+++ client/src/pages/PaymentPage/paymentinfo.jsx
@@ -2,15 +2,19 @@ import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import {
-  MDBBtn,
-  MDBContainer,
-  MDBCard,
-  MDBCardBody,
-  MDBCol,
-  MDBRow,
-  MDBInput,
+    MDBBtn,
+    MDBContainer,
+    MDBCard,
+    MDBCardBody,
+    MDBCol,
+    MDBRow,
+    MDBInput,
 }
-from 'mdb-react-ui-kit';
+    from 'mdb-react-ui-kit';
+import { WrapperPaymentInfo } from "./style";
+import { WrapperHomePage } from "../HomePage/styled";
+import { Col, Row } from "antd";
+import Header from "../../Components/Header/header";
 const PaymentInfo = () => {
     const [user, setUser] = useState({
         fullName: '',
@@ -20,45 +24,77 @@ const PaymentInfo = () => {
     });
     const onChange = event => {
         event.preventDefault();
-      setUser({ ...user, [event.target.name]: event.target.value })
+        setUser({ ...user, [event.target.name]: event.target.value })
     }
     return (
-        <div className="block-info">
-            <div className="block-box">
-                <div className="view-list">
-                    <div className="view-list__wrapper">
-                        <div className="item">
-                            <img src="../../image/logo.png"></img>
-                            <div className="item-info">
-                                <p className="item-name"></p>
-                                <div className="item-price">
-                                    <div>
-                                        <div className="box-info__box-price">
-                                            <p className="product__price--show"></p>
-                                            <p className="product__price--through"></p>
+        <div>
+            <br></br>
+            <Row>
+                <Col style={{ width: '20%', paddingLeft: '10px', overflow: 'hidden' }}>
+                </Col>
+                <Col style={{ width: '60%', paddingLeft: '10px', paddingRight: '10px' }}>
+                    <WrapperPaymentInfo>
+                        <div className="block-box">
+                            <div className="nav">
+                                <div className="nav__item1">
+                                    <span>1. Thông tin</span>
+                                </div>
+                                <div className="nav__item2">
+                                    <span>2. Thanh toán</span>
+                                </div>
+                            </div>
+                            <div className="view-list">
+                                <div className="view-list__wrapper">
+                                    <div className="item">
+                                        <img className='item__img' src="../../image/logo.png"></img>
+                                        <div className="item-info">
+                                            <p className="item-name">iphone 20</p>
+                                            <div className="item-price">
+                                                <div>
+                                                    <div className="box-info__box-price">
+                                                        <p className="product__price--show">20000000</p>
+                                                        <p className="product__price--through">20000000</p>
+                                                    </div>
+                                                </div>
+                                                <p>Số lượng:
+                                                    <span style={{ color: 'red' }}>1</span>
+                                                </p>
+                                            </div>
                                         </div>
                                     </div>
-                                    <p>Số lượng: 1</p>
+                                </div>
+                            </div>
+                            <div className="block-customer">
+                                <p>Thông tin khách hàng</p>
+                                <MDBRow>
+                                    <MDBCol col='6'>
+                                        <MDBInput wrapperClass='mb-4' label='Họ và tên' name="fullName" value={user.fullName} onChange={onChange} type='text' tabIndex="1" />
+                                    </MDBCol>
+                                    <MDBCol col='6'>
+                                        <MDBInput wrapperClass='mb-4' label='Địa chỉ' name="addRess" value={user.addRess} onChange={onChange} type='text' tabIndex="2" />
+                                    </MDBCol>
+                                </MDBRow>
+                                <MDBInput wrapperClass='mb-4' label='Số điện thoại' name="phone_number" value={user.phone_number} onChange={onChange} type='text' tabIndex="3" />
+                                <MDBInput wrapperClass='mb-4' label='Email' name="email" value={user.email} onChange={onChange} type='email' tabIndex="4" />
+                            </div>
+                            <div className="block-payment"></div>
+                            <div className="bottom-bar">
+                                <div className="total-box">
+                                    <span style={{fontWeight: '600'}}>Tổng tiền tạm tính</span>
+                                    <div className="price">
+                                        <span className="total">4000000</span>
+                                    </div>
+                                </div>
+                                <div className="btn-submit">
+                                    <button className="btn-next">Tiếp tục</button>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-                <div className="block-customer">
-                    <p>Thông tin khách hàng</p>
-                    <MDBRow>
-                        <MDBCol col='6'>
-                            <MDBInput wrapperClass='mb-4' label='Họ và tên' name="fullName" value={user.fullName} onChange={onChange} type='text' tabIndex="1" />
-                        </MDBCol>
-                        <MDBCol col='6'>
-                            <MDBInput wrapperClass='mb-4' label='Địa chỉ' name="addRess" value={user.addRess} onChange={onChange} type='text' tabIndex="2" />
-                        </MDBCol>
-                    </MDBRow>
-                    <MDBInput wrapperClass='mb-4' label='Số điện thoại' name="phone_number" value={user.phone_number} onChange={onChange} type='text' tabIndex="3" />
-                    <MDBInput wrapperClass='mb-4' label='Email' name="email" value={user.email} onChange={onChange} type='email' tabIndex="4" />
-                </div>
-                <div className="block-payment"></div>
-            </div>
+                    </WrapperPaymentInfo>
+                </Col>
+                <Col style={{ width: '20%', paddingLeft: '10px', overflow: 'hidden' }}>
+                </Col>
+            </Row>
         </div>
     )
 }
--- client/src/pages/PaymentPage/style.js
+++ client/src/pages/PaymentPage/style.js
@@ -1,6 +1,46 @@
 import styled from "styled-components";
 
 export const WrapperPaymentInfo = styled.div`
+    .nav {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: #f4f6f8;
+        gap: 10px;
+        left: 0;
+        margin-bottom: 10px;
+        padding: 5px 15px 10px;
+        position: sticky;
+        top: 0;
+        width: 100%;
+        z-index: 10;
+        flex-wrap: wrap;
+        list-style: none;
+    }
+    .nav__item1{
+        border-bottom: 3px solid #d70018;
+        color: #d70018;
+        pointer-events: none;
+        cursor: pointer;
+        flex: 1;
+        font-weight: 600;
+        padding: 5px 0;
+        text-transform: uppercase;
+        display: flex;
+        justify-content: center;
+        list-style: none;
+    }
+    .nav__item2{
+        border-bottom: 3px solid #919eab;
+        color: #919eab;
+        cursor: pointer;
+        flex: 1;
+        font-weight: 600;
+        padding: 5px 0;
+        text-transform: uppercase;
+        display: flex;
+        justify-content: center;
+    }
     .view-list{
         background-color: #fff;
         border: 1px solid rgba(145,158,171,.239);
@@ -9,6 +49,125 @@ export const WrapperPaymentInfo = styled.div`
         width: 100%;
     }
     .view-list__wrapper{
-        
+        display: flex;
+        flex-direction: column;
+        width: 100%;
     }
+    .item{
+        border-bottom: 0 solid rgba(145,158,171,.24);
+        display: flex;
+        gap: 15px;
+        overflow: hidden;
+        padding: 12px 0;
+        transition: .2s ease-in-out;
+        width: 100%;
+    }
+    .item__img{
+        height: 100px;
+        width: 100px;
+        flex-shrink: 0;
+    }
+    img {
+        display: block;
+        height: auto;
+        max-width: 100%;
+        vertical-align: middle;
+    }
+    .item-info {
+        display: flex;
+        flex: 1;
+        flex-direction: column;
+        gap: 5px;
+    }
+    .item-name {
+        color: #111;
+        font-size: 16px;
+        font-weight: 500;
+    }
+    .item-price {
+        align-items: flex-end;
+        display: flex;
+        flex-wrap: wrap;
+        gap: 7px;
+        justify-content: space-between;
+    }
+    .box-info__box-price {
+        align-items: flex-end;
+        display: flex;
+        flex-wrap: wrap;
+        gap: 8px;
+    }
+    .product__price--show {
+        color: #d70018;
+        display: inline-block;
+        font-size: 17px;
+        font-weight: 500;
+        line-height: 1;
+    }
+    .product__price--through {
+        color: #707070;
+        display: inline-block;
+        font-size: 14px;
+        font-weight: 500;
+        position: relative;
+        -webkit-text-decoration: line-through;
+        text-decoration: line-through;
+        top: 2px;
+    }
+    .block-customer{
+        padding: 10px;
+    }
+    .block-customer p{
+        text-transform: uppercase;
+    }
+    .bottom-bar{
+        background-color: #fff;
+        border: 1px solid rgba(145,158,171,.239);
+        border-top-left-radius: 5px;
+        border-top-right-radius: 5px;
+        bottom: 0;
+        box-shadow: 0 -4px 20px -1px rgba(40,124,234,.15);
+        width: 60%;
+        padding: 10px 10px 20px;
+        position: fixed;
+        right: 50%;
+        transform: translateX(50%);
+        z-index: 20;
+        margin-top: auto;
+        margin-left: auto;
+    margin-right: auto;
+    }
+    .total-box{
+        align-items: flex-start!important;
+        justify-content: space-between!important;
+        display: flex!important;
+    }
+    .price{
+        align-items: flex-end!important;
+        column!important;
+        display: flex!important;
+    }
+    .total {
+        color: #d70018;
+    font-weight: 700;
+    }
+    .btn-submit {
+        margin-top: 0.5rem!important;
+    }
+    .btn-next {
+        background-color: #d70018;
+        font-weight: 500;
+        cursor: pointer;
+        width: 100%!important;
+        align-items: center!important;
+        justify-content: center!important;
+        flex-direction: column!important;
+        display: flex!important;
+        border-color: #dc3545;
+        color: #fff;
+        border: 1px solid transparent;
+    }
+`
+export const WrapperBtnNext = styled.div`
+
 `
