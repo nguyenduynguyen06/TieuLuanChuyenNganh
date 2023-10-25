d6c2e7466b51e467d61c4cfebb119c7b045861f7
QuangDuongLe <20110454@student.hcmute.edu.vn>
2023-10-25 17:27:11 +0700

    updateOrder

--- server/controller/OrderController.js
+++ server/controller/OrderController.js
@@ -24,9 +24,9 @@ const addOrder = async (req, res) => {
   try {
     const orderCode = await generateRandomOrderCode();
     const { userId } = req.params;
-    const { userName, userEmail, address,shippingMethod, paymentMethod, subTotal, totalPay, voucher,userPhone } = req.body;
+    const { userName, userEmail, address, shippingMethod, paymentMethod, subTotal, totalPay, voucher, userPhone } = req.body;
     const cart = await Cart.findOne({ user: userId }).populate('items.productVariant');
-    
+
     if (!cart || !cart.items || cart.items.length === 0) {
       return res.status(400).json({ success: false, error: 'Giỏ hàng không tồn tại hoặc trống' });
     }
@@ -47,9 +47,9 @@ const addOrder = async (req, res) => {
       voucher,
       userPhone
     });
-    
+
     for (const cartItem of cart.items) {
-      const productVariantId = cartItem.productVariant; 
+      const productVariantId = cartItem.productVariant;
       const productVariant = await ProductVariant.findById(productVariantId);
       if (!productVariant) {
         return res.status(404).json({ success: false, error: 'Sản phẩm biến thể không tồn tại' });
@@ -66,7 +66,7 @@ const addOrder = async (req, res) => {
         await productVariant.save();
       }
     }
-    
+
     await Cart.updateOne({ user: userId }, { $set: { items: [] } });
     const savedOrder = await newOrder.save();
     res.status(201).json({ success: true, data: savedOrder });
@@ -75,40 +75,70 @@ const addOrder = async (req, res) => {
     <html>
     <head>
         <style>
-        body {
-          font-family: Arial, sans-serif;
-          background-color: #f4f4f4;
-          margin: 0;
-          padding: 0;
-      }
-      h1 {
-          color: #333;
-      }
-      ul {
-          list-style: none;
-          padding: 0;
-      }
-      li {
-          margin: 10px 0;
-      }
+          body {
+              font-family: Arial, sans-serif;
+          }
+          h2 {
+              text-align: center;
+          }
+          .order-info {
+              background-color: #f2f2f2;
+              padding: 10px;
+              margin: 20px 0;
+          }
+          h1 {
+              
+          }
+          ul {
+              list-style: none;
+              padding: 0;
+          }
+          li {
+              margin: 10px 0;
+          }
+          .product-item {
+            display: flex;
+            margin-bottom: 20px;
+          }
+          .product-image {
+              max-width: 100px;
+              margin-right: 10px;
+          }
+          table {
+              width: 100%;
+              border-collapse: collapse;
+          }
+          table, th, td {
+              border: 1px solid #ccc;
+          }
+          th, td {
+              padding: 8px;
+              text-align: left;
+          }
+          th {
+              background-color: #f2f2f2;
+          }
         </style>
     </head>
     <body>
-    <table style="background-color: #f4f4f4; font-family: Arial, sans-serif;">
-    <tr>
-    <td>
-        <h1>Đơn hàng của bạn</h1>
-        <p>Mã đơn hàng: ${orderCode}</p>
-        <p>Tên người đặt hàng: ${userName}</p>
-        <p>Nơi nhận hàng: ${address}</p>
-        <p>Số điện thoại người đặt hàng: ${userPhone}</p>
-        <p>Tổng tiền thanh toán: ${new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(totalPay)}</p>
-        </td>
-        </tr>
-        </table>
+    <div class="container" style="max-width: 600px; margin: 0 auto; padding: 20px;">
+      <div style="display: flex; align-items: center;">
+        <img src="https://res.cloudinary.com/doq4spvys/image/upload/v1698228631/mqjpap63hv6bxuddgfbx.png" style="width: 40px; height: 40px; margin-right: 10px;"/>
+        <span style="line-height: 40px; font-size: 17px; font-weight: 600"> Di Động Gen Z</span> 
+      </div>
+        <p>Di Động Gen Z chân thành cảm ơn sự ủng hộ của bạn, chúc bạn có trải nghiệm mua sắm thật vui vẻ cùng Di Động Gen Z!</p>
+        <h2 style="color: #d70018;">Đơn hàng của bạn</h2>
+        <div class="order-info">
+          <p><strong>Mã đơn hàng:</strong> ${orderCode}</p>
+          <p><strong>Tên khách hàng:</strong> ${userName}</p>
+          <p><strong>Địa chỉ nhận hàng:</strong> ${address}</p>
+          <p><strong>Số điện thoại:</strong> ${userPhone}</p>
+          <p><strong>Tổng tiền thanh toán:</strong> ${new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(totalPay)}</p>
+        </div>
 `;
 
-        emailHTML += `
+    emailHTML += `
+        <h2>Sản phẩm</h2>
         <table style="border-collapse: collapse; width: 100%;">
         <tr>
           <th style="border: 1px solid #000;">Tên sản phẩm</th>
@@ -116,7 +146,7 @@ const addOrder = async (req, res) => {
           <th style="border: 1px solid #000;">Số lượng</th>
           <th style="border: 1px solid #000;">Màu sắc</th>
           <th style="border: 1px solid #000;">Đơn giá</th>
-          <th style="border: 1px solid #000;">Tổng tiền</th>
+          <th style="border: 1px solid #000;">Thành tiền</th>
         </tr>
         <!-- Duyệt qua danh sách sản phẩm đã đặt -->
         ${populatedOrder.items.map(item => `
@@ -124,22 +154,23 @@ const addOrder = async (req, res) => {
           <td style="border: 1px solid #000; text-align: center;">
           ${item.product.name}  ${item.memory !== undefined ? item.memory : ''}
         </td>        
-            <td style="border: 1px solid #000; text-align: center;"><img src="${item.pictures}" alt="Hình ảnh sản phẩm" style="width: 100px; height: 100px;"></td>
+            <td style="border: 1px solid #000; text-align: center;"><img src="${item.pictures}" alt="Hình ảnh sản phẩm" class="product-image" style="width: 100px; height: 100px;"></td>
             <td style="border: 1px solid #000; text-align: center;">${item.quantity}</td>
             <td style="border: 1px solid #000; text-align: center;">${item.color}</td>
             <td style="border: 1px solid #000; text-align: center;">${new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(item.price)}</td>
             <td style="border: 1px solid #000; text-align: center;">${new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(item.subtotal)}</td>
           </tr>
         `).join('')}
-      </table>      
+      </table>   
+      </div>   
           `;
 
-const data = {
-  email: userEmail,
-  html: emailHTML,
-};
+    const data = {
+      email: userEmail,
+      html: emailHTML,
+    };
 
-await orderSendMail(data);
+    await orderSendMail(data);
   } catch (error) {
     console.error('Lỗi khi thêm đơn hàng:', error);
     res.status(500).json({ success: false, error: error.message });
@@ -148,7 +179,7 @@ await orderSendMail(data);
 const updateOrderStatus = async (req, res) => {
   try {
     const { orderId } = req.params;
-    const { newStatus } = req.body; 
+    const { newStatus } = req.body;
 
     const updatedOrder = await Order.findByIdAndUpdate(orderId, { status: newStatus }, { new: true });
 
@@ -244,4 +275,4 @@ const cancelOrder = async (req, res) => {
   }
 };
 
-module.exports = { addOrder,updateOrderStatus,getCompletedOrders,getOrdersByUserId,getOrdersHomeDelivery,getOrdersStorePickup,getOrdersWaitingForConfirmation,cancelOrder };
+module.exports = { addOrder, updateOrderStatus, getCompletedOrders, getOrdersByUserId, getOrdersHomeDelivery, getOrdersStorePickup, getOrdersWaitingForConfirmation, cancelOrder };
